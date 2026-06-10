390420b7bf3a1e2a21563b3cb29a543e201ebf2a
Rohan Pal <pal.rohan0408@gmail.com>
2026-06-10 19:27:50 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import  logging
-from networksecurity.entity.config_entity import DataIngetionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngetionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_transformation import DataTransformation
 
 import sys 
 
@@ -28,6 +29,13 @@ if __name__ == "__main__":
         logging.info("data validation completed")
 
         print(data_validation_artifact)
+
+        logging.info("Initate the data trasnformation")
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transfomration = DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact = data_transfomration.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data transformation completed")
 
         
     
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -23,3 +23,22 @@ def write_yaml_file(file_path: str, content: object, replace : bool= False)->Non
                 yaml.dump(content,f)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+    
+def save_numpy_array_data(file_path: str, array:np.array):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as f:
+            np.save(f,array)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def save_object(file_path: str,obj : object)->None:
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as f:
+            pickle.dump(obj,f)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
