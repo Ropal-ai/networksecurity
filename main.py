764024dd1c660bcec249acd709a21d05e7c4a18b
Rohan Pal <pal.rohan0408@gmail.com>
2026-06-11 18:40:38 +0530

    Model trainer evaluator and mlflow commited

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from networksecurity.entity.config_entity import DataIngetionConfig,DataValidati
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
 
 import sys 
 
@@ -13,22 +15,20 @@ if __name__ == "__main__":
         trainingpipelineconfig = TrainingPipelineConfig()
         dataingestionconfig = DataIngetionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
-
         logging.info("Initate the data ingestion config")
-
         dataingestion_artifact = data_ingestion.Initiate_data_ingestion()
-
         logging.info("Data ingestion completed")
         print(dataingestion_artifact)
+        
+
 
         data_validation_config = DataValidationConfig(trainingpipelineconfig)
         data_validation = DataValidation(dataingestion_artifact,data_validation_config)
         logging.info("Initate the data validation")
-
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("data validation completed")
-
         print(data_validation_artifact)
+
 
         logging.info("Initate the data trasnformation")
         data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
@@ -36,6 +36,13 @@ if __name__ == "__main__":
         data_transformation_artifact = data_transfomration.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data transformation completed")
+
+        logging.info("Model Training started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
 
         
     
